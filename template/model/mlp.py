232a1068098a6575c9fc5467a3db04583ec7672c
Jacod97 <wotlr6894@naver.com>
2026-05-03 22:59:05 +0900

    init & MLP, CNN

---
+++ template/model/mlp.py
@@ -0,0 +1,20 @@
+import torch.nn as nn
+from torch import Tensor
+
+
+class MLP(nn.Module):
+    """다층 퍼셉트론 분류기"""
+    def __init__(self, input_size: int, hidden_size: int, num_classes: int):
+        super().__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, hidden_size)
+        self.fc3 = nn.Linear(hidden_size, num_classes)
+        self.relu = nn.ReLU()
+
+    def forward(self, x: Tensor) -> Tensor:
+        out = self.fc1(x)
+        out = self.relu(out)
+        out = self.fc2(out)
+        out = self.relu(out)
+        out = self.fc3(out)
+        return out
